a2ebe7dd5996d63df565bd26b742c7a7c8940e17
User <gold.like.simple@gmail.com>
2023-05-18 13:36:22 +1000

    [+] Program & Startup with DI

---
+++ WowApp.Grpc.Server/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WowApp.Database;
+using WowApp.Database.User;
+using WowApp.Grpc.Services;
+
+namespace WowApp.Grpc;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var typeOfContent = typeof(Startup);
+
+        services.AddDbContext<PostgresContext>(
+            options => options.UseNpgsql(
+                Configuration.GetConnectionString("WowDB"), b => b.MigrationsAssembly(typeOfContent.Assembly.GetName().Name)
+            )
+        );
+
+        services.AddGrpc();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IDatabaseContainer, DatabaseContainer>();
+    }
+
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, PostgresContext dbContext)
+    {
+        if (env.IsDevelopment()) { }
+
+        app.UseRouting();
+        app.UseEndpoints(
+            endpoints =>
+            {
+                endpoints.MapGrpcService<UserService>();
+            });
+    }
+}
